d74f909d19d223035d9e021c8c2da519c4353daf
sameza <samezarus@gmail.com>
2018-12-05 12:30:59 +1000

    Update RealDescTop.ps1

--- RealDescTop.ps1
+++ RealDescTop.ps1
@@ -10,8 +10,13 @@ $path2 = 'c:\Users\'+$user+'\desktop\'+$linkName+'.lnk'
 
 if (Test-Path -Path $path1)
 {
+    # Ярлык на рабочий стол
     $shell = New-Object -comObject Wscript.Shell
     $shortcut = $shell.CreateShortcut($path2)
     $shortcut.TargetPath = $path1
     $shortcut.Save()
+
+    # В раздел "Быстрый доступ" (win10/srv16)
+    $o = new-object -com shell.application
+    $o.Namespace($path1).Self.InvokeVerb('pintohome')
 }
